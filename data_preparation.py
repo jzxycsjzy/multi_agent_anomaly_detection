97a8fe98ee40aefba7f9c643175ec64bf7aba7e6
jzxycsjzy <jzxycsjzy@gmail.com>
2023-03-09 13:31:22 +0800

    update

--- data_preparation.py
+++ data_preparation.py
@@ -1,6 +1,11 @@
 import os
 import sys
+import time
+import pandas as pd
+import datetime
 import logging
+
+from tqdm import tqdm
 
 # Import drain3 model
 from drain3.template_miner import TemplateMiner
@@ -16,16 +21,19 @@ def Drain_Init():
     ph = PersistenceHandler()
     logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(message)s')
     drain3Config = TemplateMinerConfig()
-    drain3Config.load('./drain3/drain3/ini')
+    drain3Config.load('./config/drain3.ini')
     drain3Config.profiling_enabled = True
 
     tmp = TemplateMiner(config=drain3Config, persistence_handler=ph)
     return tmp
 
-def TraceLogCombine(tmp: TemplateMiner):
+def TraceLogCombine(tmp: TemplateMiner, trace_list: list, log_file: str, tgt_file: str):
     """
     params:
         tmp: drain3 template miner object
+        trace_list: list of trace files
+        log_list: list of log files
+        tgt_file: file to save trace-log data
 
     TODO:
         1. Read log data and trace data and combine them together.
@@ -34,15 +42,25 @@ def TraceLogCombine(tmp: TemplateMiner):
                 Tips: it will be better if there is a function could be used for checking DAG.
         2. drain3 template will be mined at the same time.
     """         
-    data_dir = "data/TraceLogData"
-    pass
+    print("######################")
+    print(trace_list, log_file, tgt_file)
+    log_f = open(log_file, 'r')
+    log_lines = log_f.readlines()
+    log_f.close()
 
-def Unzip_Data():
-    os.system("cd data/TraceLogData")
-    for i in range(1, 15):
-        s = "%02d" % i
-        os.system("pwd")
-        os.system("cd data/TraceLogData; ./unzip.sh " + s)
+    for trace_file in trace_list:
+        trace_lines = pd.read_csv(trace_file)
+        trace_id = ""
+        count = 0
+        logs = pd.DataFrame([], columns=['traceid', 'index', 'spanid', 'parentspanid', 'loginfo'])
+        logs.loc[logs.shape[0]] = [0,1,2,3,4]
+        print(logs)
+        for i in tqdm(range(trace_lines.shape[0])):
+            line = trace_lines.loc[i]
+            
+        exit()
+
+
 
 def WorkFlow():
     """
@@ -50,8 +68,55 @@ def WorkFlow():
     """
     # Create drain template miner.
     drain3_template = Drain_Init()
+    data_dir = "./data/DeepTraLog/TraceLogData"
+    tgt_dir = "./data/PreprocessedData"
 
+    tgt_file = ""
+    # Read All data
+    for f_0 in os.listdir(data_dir):
+        if "F" in f_0:
+            tgt_file = f_0[:3]
+        else:
+            tgt_file = "normal"
+        tgt_file = os.path.join(tgt_dir, tgt_file)
+        log_file = ""
+        trace_files = []
+        f_1 = os.path.join(data_dir, f_0)
+        for f_2 in os.listdir(f_1):
+            f_3 = os.path.join(f_1, f_2)
+            # If has sub folder
+            if os.path.isdir(f_3):
+                tf = []
+                lf = ""
+                for f_4 in os.listdir(f_3):
+                    f_5 = os.path.join(f_3, f_4)
+                    if f_4.endswith("csv"):
+                        tf.append(f_5)
+                    else:
+                        lf = f_5
+                TraceLogCombine(drain3_template, tf, lf, tgt_file)
+            else:
+                # If has no sub folder
+                if f_3.endswith("csv"):
+                    trace_files.append(f_3)
+                else:
+                    log_file = f_3
+        if log_file != "":
+            TraceLogCombine(drain3_template, trace_files, log_file, tgt_file)
+
+# Tools
+def Time2Timestamp(timestr: str):
+    """
+    Transfer time from format YYMMDD HHMMSS to timestamp
+    """
+    datetime_obj = datetime.datetime.strptime(timestr, "%Y-%m-%d %H:%M:%S.%f")
+    timestamp = int(time.mktime(datetime_obj.timetuple()) * 1000 + datetime_obj.microsecond / 1000)
+    return timestamp
+
+def LogLineSplit(logline: str):
+    pass
 
 if __name__ == '__main__':
-    Unzip_Data()
+    WorkFlow()
+    pass
 
