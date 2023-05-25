cb2654f6fe193094af998d7ef1bb4d8943b23f6c
Jayce Tan <60682510+jzxycsjzy@users.noreply.github.com>
2023-05-25 14:28:02 +0800

    Update SecondTimeClassification.py

--- SecondTimeClassification.py
+++ SecondTimeClassification.py
@@ -11,8 +11,7 @@ from sklearn.metrics import recall_score, precision_score
 from tqdm import tqdm
 import numpy as np
 import sys
-__stdout__ = sys.stdout # 标准输出就用这行
-sys.stdout = open('log3.txt', 'a')
+
 
 def CheckData(file = "res.txt"):
     data = []
@@ -87,10 +86,10 @@ if __name__ == '__main__':
     
     data, labels = CheckData("res.txt")
     train_data, train_label = CheckData("log.txt")
-    # 划分训练集和测试集
+    # split train_set and test_set
     X_train, _, y_train, _ = train_test_split(data, labels, test_size=0.05, random_state=1002)
     X_test, _, y_test, _ = train_test_split(train_data, train_label, test_size=0.05, random_state=1002)
-    # 训练模型
+    # Train the model
     # clf2 = HistGradientBoostingClassifier(learning_rate=0.05, max_iter=300, l2_regularization=0.2, min_samples_leaf=2, verbose=1, max_leaf_nodes=128)
     # clf2.fit(X_train, y_train)
     # scores = clf2.score(X_test, y_test)
@@ -109,48 +108,4 @@ if __name__ == '__main__':
     p = precision_score(y_test, res, pos_label=0)
     print('Accuracy:', scores) # 0.9694
     print('Recall: ', r) # 0.9946
-    print('Precision:', p)
-
-    clf2 = HistGradientBoostingClassifier(learning_rate=0.05, max_iter=300, l2_regularization=0.3, min_samples_leaf=2, verbose=1, max_leaf_nodes=256, early_stopping=False)
-    clf2.fit(X_train, y_train)
-    scores = clf2.score(X_test, y_test)
-    scores.mean()
-    res = clf2.predict(X_test)
-    r = recall_score(y_test, res, pos_label=0)
-    p = precision_score(y_test, res, pos_label=0)
-    print('Accuracy:', scores)
-    print('Recall: ', r)
-    print('Precision:', p)
-
-    clf2 = HistGradientBoostingClassifier(learning_rate=0.05, max_iter=300, l2_regularization=0.35, min_samples_leaf=1, verbose=1, max_leaf_nodes=256, early_stopping=False)
-    clf2.fit(X_train, y_train)
-    scores = clf2.score(X_test, y_test)
-    scores.mean()
-    res = clf2.predict(X_test)
-    r = recall_score(y_test, res, pos_label=0)
-    p = precision_score(y_test, res, pos_label=0)
-    print('Accuracy:', scores)
-    print('Recall: ', r)
-    print('Precision:', p)
-
-    clf2 = HistGradientBoostingClassifier(learning_rate=0.05, max_iter=300, l2_regularization=0.3, min_samples_leaf=1, verbose=1, max_leaf_nodes=256, early_stopping=False)
-    clf2.fit(X_train, y_train)
-    scores = clf2.score(X_test, y_test)
-    scores.mean()
-    res = clf2.predict(X_test)
-    r = recall_score(y_test, res, pos_label=0)
-    p = precision_score(y_test, res, pos_label=0)
-    print('Accuracy:', scores)
-    print('Recall: ', r)
-    print('Precision:', p)
-    
-    clf2 = HistGradientBoostingClassifier(learning_rate=0.05, max_iter=300, l2_regularization=0.4, min_samples_leaf=3, verbose=1, max_leaf_nodes=256)
-    clf2.fit(X_train, y_train)
-    scores = clf2.score(X_test, y_test)
-    scores.mean()
-    res = clf2.predict(X_test)
-    r = recall_score(y_test, res, pos_label=0)
-    p = precision_score(y_test, res, pos_label=0)
-    print('Accuracy:', scores)
-    print('Recall: ', r)
     print('Precision:', p)
